10ed313ca41d68bb268b61129066e8b832b7e7ec
Alex <alexandre.d.paquette@gmail.com>
2023-11-29 16:06:52 +0000

    Refactor: Moved VISITOR carparkind check to the start of calculate method to allow for early exit and remove unnecessary assignment of normal and reduced rate horus

--- VerificationProject/src/cm/Rate.java
+++ VerificationProject/src/cm/Rate.java
@@ -88,9 +88,9 @@ public class Rate {
         return isValid;
     }
     public BigDecimal calculate(Period periodStay) {
+        if (this.kind==CarParkKind.VISITOR) return BigDecimal.valueOf(0.0);
         int normalRateHours = periodStay.occurences(normal);
         int reducedRateHours = periodStay.occurences(reduced);
-        if (this.kind==CarParkKind.VISITOR) return BigDecimal.valueOf(0.0);
         return (this.hourlyNormalRate.multiply(BigDecimal.valueOf(normalRateHours))).add(
                 this.hourlyReducedRate.multiply(BigDecimal.valueOf(reducedRateHours)));
     }
