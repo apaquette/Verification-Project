56ab53f035f39c3c6aac02bd2baa4d453fb5c4f5
Alex <alexandre.d.paquette@gmail.com>
2023-11-29 20:19:52 +0000

    Test: Fixed incorrect period for Test Case 18

--- VerificationProject/src/cm/PaquetteAlexTestTask3.java
+++ VerificationProject/src/cm/PaquetteAlexTestTask3.java
@@ -276,7 +276,7 @@ class PaquetteAlexTestTask3 {
 
     @Test @DisplayName("Rate calculate: Free for VISITOR up to 10")
     void RateCalculate_BlackBox_TestCase18(){
-        assertEquals(new BigDecimal("0"), testRate_Visitor.calculate(testPeriod));
+        assertEquals(new BigDecimal("0"), testRate_Visitor.calculate(new Period(0,10)));
     }
 
     //BLACK BOX TESTING END
