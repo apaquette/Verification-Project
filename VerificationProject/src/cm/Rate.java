64eb71d4ec89ffc593c2b48c16e08658742d18c7
Alex <alexandre.d.paquette@gmail.com>
2023-12-06 16:43:49 +0000

    Refactor: Implement rate calculator for in Rate for STUDENT. Passes all tests.

--- VerificationProject/src/cm/Rate.java
+++ VerificationProject/src/cm/Rate.java
@@ -107,10 +107,11 @@ public class Rate {
                 }
                 return total.subtract(BigDecimal.valueOf(10)).multiply(BigDecimal.valueOf(0.5)).stripTrailingZeros();*/
             case CarParkKind.STUDENT:
-                if(total.compareTo(BigDecimal.valueOf(5.5)) > 0){
+                return rateCalculator.calculate(periodStay, normalRateHours, reducedRateHours, this.hourlyNormalRate, this.hourlyReducedRate).stripTrailingZeros();
+                /*if(total.compareTo(BigDecimal.valueOf(5.5)) > 0){
                     return total.subtract(BigDecimal.valueOf(5.5)).multiply(BigDecimal.valueOf(0.66)).add(BigDecimal.valueOf(5.5)).stripTrailingZeros();
                 }
-                break;
+                break;*/
             case CarParkKind.STAFF:
                 if(total.compareTo(BigDecimal.valueOf(10)) > 0){
                     return BigDecimal.valueOf(10);
